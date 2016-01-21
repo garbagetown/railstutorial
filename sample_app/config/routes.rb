0330cd089cc8e14619bf5ce6021fbf7e1b5b6697
garbagetown <garbagetown@gmail.com>
2016-01-21 22:42:06 +0900

    fix typo

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/heome'
+  get 'static_pages/home'
 
   get 'static_pages/help'
 
--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get heome" do
-    get :heome
+  test "should get home" do
+    get :home
     assert_response :success
   end
 
