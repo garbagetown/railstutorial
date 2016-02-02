429a23761126612046676325fa2055612213e75e
yuichiro umezawa <garbagetown@gmail.com>
2016-02-02 19:18:12 +0900

    add user resource

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,26 +1,25 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
-  root 'static_pages#home'
-  get 'help' => 'static_pages#help'
-  get 'about' => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
-  get 'signup' => 'users#new'
-  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup' => 'users#new'
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :users  
 
   # Example resource route with options:
   #   resources :products do
